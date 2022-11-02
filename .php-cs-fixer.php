c191c32737a670e305d8666e1999a20035cf6443
Jeroen de Graaf <hello@jero.work>
2022-11-02 07:21:42 +0100

    Update phpcsfixer configuration

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -19,5 +19,11 @@ return (new PhpCsFixer\Config())
         'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
         'php_unit_test_class_requires_covers' => false,
         'phpdoc_order' => true,
+        'phpdoc_align' => ['align' => 'left'],
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => null,
+            'import_functions' => null,
+        ],
     ])
     ->setFinder($finder);
